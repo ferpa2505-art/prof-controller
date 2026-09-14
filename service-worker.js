73cc39b3a61d0e1b6e3490d3a8bba2707aebf039
Luis Fernando Paulino <ferpa2505@gmail.com>
2026-09-14 10:21:07 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -5,8 +5,8 @@ const ASSETS = [
   './styles.css',
   './app.js',
   './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
+  './icon-192.png',
+  './icon-512.png'
 ];
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
