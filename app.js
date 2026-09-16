900aec92072cc910815147e329830f6cfa465dee
Luis Fernando Paulino <ferpa2505@gmail.com>
2026-09-16 12:58:24 +0200

    Update app.js

--- app.js
+++ app.js
@@ -7052,9 +7052,9 @@ async function init() {
   showTab('dashboard');
   startNewsSchedule();
 
-   init();
-  if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('./service-worker.js');
-  });
+   if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('./service-worker.js').catch(() => {});
+  }
 }
+
+   init();
