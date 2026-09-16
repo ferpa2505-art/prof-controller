d060292ee80bcbb6045111238ad786497f5310a2
Luis Fernando Paulino <ferpa2505@gmail.com>
2026-09-16 14:38:30 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE = 'prof-controller-v29'; // SEMPRE incremente ao publicar mudanças
+const CACHE = 'prof-controller-v30'; // SEMPRE incremente ao publicar mudanças
 const ASSETS = [
   './',
   './index.html',
-  './styles.css?v=29',
-  './app.js?v=29',
+  './styles.css?v=30',
+  './app.js?v=30',
   './manifest.json',
   './icon-192.png',
   './icon-512.png'
